3c70ae98ef0866f72532c6dcc534cdff5a22366d
Yohann BREDOUX <ybr@zenexity.com>
2012-02-24 15:54:44 +0100

    UI

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,10 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 import play.api.data.validation.Constraints._
-
 import models._
+import service._
+import models.Poll
+import models.Poll
 
 object Application extends Controller {
   val createForm: Form[Poll] = Form(
@@ -27,14 +29,24 @@ object Application extends Controller {
   }
   
   def createPost = Action { implicit request =>
-    createForm.bindFromRequest.fold(
+    val form = createForm.bindFromRequest
+    form.fold(
         errors => BadRequest(views.html.create(errors)),
-        poll => Ok(views.html.mock("Poll created"))
+        poll => {
+          Service.createPoll(poll.name, poll.description, poll.alternatives.map(_.name)) match {
+            case Some(poll) => {
+              Redirect(routes.Application.voteGet(poll.name)).flashing("success" -> "Your poll %s has been created.".format(poll.name))
+            }
+            case None => BadRequest(views.html.create(form))
+          }
+        }
     )
   }
   
-  def voteGet(name: String) = Action {
-    Ok(views.html.mock("voteGet " + name))
+  def voteGet(name: String) = Action { implicit request =>
+    //val poll = Service.findPoll()
+    val poll = new Poll("name", "description", Nil, Nil);
+    Ok(views.html.vote(poll))
   }
   
   def result(name: String) = Action {
