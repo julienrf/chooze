933e598746360eca5a75e9c593ec90946aaaeff9
Julien Richard-Foy <julien@richard-foy.fr>
2012-02-28 19:28:22 +0100

    Move generateUniqueSlug in Util object

--- app/service/Service.scala
+++ app/service/Service.scala
@@ -3,25 +3,12 @@ package service
 import db.Db
 import models._
 import util.Util
-import scala.annotation.tailrec
 
 object Service {
 
   def createPoll(name: String, description: String, alternatives: Seq[String]): Option[Long] = {
-    @tailrec
-    def generateUniqueSlug(slug: String, existingSlugs: Seq[String]): String = {
-      if (!(existingSlugs contains slug)) {
-        slug
-      } else {
-        val EndsWithNumber = "(.+-)([0-9]+)$".r
-        slug match {
-          case EndsWithNumber(s, n) => generateUniqueSlug(s + (n.toInt + 1), existingSlugs)
-          case s => generateUniqueSlug(s + "-2", existingSlugs)
-        }
-      }
-    }
-    // TODO Do not fetch all the slugs until necessary
-    Db.Poll.create(name, generateUniqueSlug(Util.slugify(name.take(42)), Db.Poll.slugs), description, alternatives)
+    // TODO Do not fetch all the slugs unless necessary
+    Db.Poll.create(name, Util.generateUniqueSlug(Util.slugify(name.take(42)), Db.Poll.slugs), description, alternatives)
   }
   
   def findPoll(slug: String): Option[Poll] = Db.Poll.find(slug)
--- app/util/Util.scala
+++ app/util/Util.scala
@@ -1,8 +1,23 @@
 package util
 
+import scala.annotation.tailrec
+
 object Util {
   def slugify(str: String): String = {
     import java.text.Normalizer
     Normalizer.normalize(str, Normalizer.Form.NFD).replaceAll("[^\\w ]", "").replace(" ", "-").toLowerCase
+  }
+  
+  @tailrec
+  def generateUniqueSlug(slug: String, existingSlugs: Seq[String]): String = {
+    if (!(existingSlugs contains slug)) {
+      slug
+    } else {
+      val EndsWithNumber = "(.+-)([0-9]+)$".r
+      slug match {
+        case EndsWithNumber(s, n) => generateUniqueSlug(s + (n.toInt + 1), existingSlugs)
+        case s => generateUniqueSlug(s + "-2", existingSlugs)
+      }
+    }
   }
 }
