bd4a2d993bdb00da36b6aea503e4ef7607255141
Julien Richard-Foy <julien@richard-foy.fr>
2012-03-03 17:15:57 +0100

    Disable caching in Dev mode, Use local cache as well as browser cache

--- app/controllers/BrowserCache.scala
+++ app/controllers/BrowserCache.scala
@@ -4,13 +4,16 @@ import play.api.mvc.{Result, PlainResult, RequestHeader, Results}
 import Results.NotModified
 import play.api.http.HeaderNames.{IF_NONE_MATCH, ETAG}
 import play.api.libs.Codecs
+import play.api.cache.Cache
+import play.api.Play.current
+import play.api.Mode
 
 import java.util.Date
 
 trait BrowserCache {
 
   private val etags = collection.mutable.Map.empty[String, String]
-  
+
   private def etagFor(identifier: String): String = {
     etags.get(identifier).getOrElse {
       val etag = Codecs.sha1(identifier)
@@ -20,13 +23,19 @@ trait BrowserCache {
   }
 
   def browserCached(lastModified: Date)(result: => PlainResult)(implicit request: RequestHeader): Result = {
-    val resourceEtag = etagFor(request.uri + " - " + lastModified.getTime)
-    request.headers.get(IF_NONE_MATCH) match {
-      case Some(etag) if etag == resourceEtag => NotModified
-      case _ => result.withHeaders(ETAG -> resourceEtag)
+    if (current.mode == Mode.Dev) {
+      result
+    } else {
+      val resourceEtag = etagFor(request.uri + " - " + lastModified.getTime)
+      request.headers.get(IF_NONE_MATCH) match {
+        case Some(etag) if etag == resourceEtag => NotModified
+        case _ => Cache.getOrElse(resourceEtag) {
+          result.withHeaders(ETAG -> resourceEtag)
+        }
+      }
     }
   }
-  
+
   def browserCached(result: => PlainResult)(implicit request: RequestHeader): Result = {
     browserCached(new Date(0))(result)
   }
