fb2337a9c399aa64fa794f36cdc319a945e4536b
Julien Richard-Foy <julien@richard-foy.fr>
2012-03-10 17:06:04 +0100

    Make cache language dependant

--- app/controllers/Cache.scala
+++ app/controllers/Cache.scala
@@ -6,10 +6,38 @@ import play.api.http.HeaderNames.{IF_NONE_MATCH, ETAG}
 import play.api.libs.Codecs
 import play.api.Play.current
 import play.api.Mode
-
 import java.util.Date
+import play.api.Logger
+import play.api.i18n.Lang
 
 trait Cache {
+
+  def Cached(lastModified: Date)(result: => PlainResult)(implicit request: RequestHeader, lang: Lang): Result = {
+    if (current.mode == Mode.Dev) { // Chuis deg.
+      result
+    } else {
+      val resourceEtag = etagFor(request.uri + "-" + lang + "-" + lastModified.getTime)
+      request.headers.get(IF_NONE_MATCH) match {
+        case Some(etag) if etag == resourceEtag => {
+          Logger.debug("'%s' Not Modified")
+          NotModified
+        }
+        case _ => {
+          play.api.cache.Cache.getOrElse {
+            Logger.debug("Get '%s' from cache".format(request.uri))
+            resourceEtag
+          } {
+            Logger.debug("Compute '%s'".format(request.uri))
+            result.withHeaders(ETAG -> resourceEtag)
+          }
+        }
+      }
+    }
+  }
+
+  def Cached(result: => PlainResult)(implicit request: RequestHeader, lang: Lang): Result = {
+    Cached(new Date(0))(result)
+  }
 
   private val etags = collection.mutable.Map.empty[String, String]
 
@@ -19,23 +47,5 @@ trait Cache {
       etags += identifier -> etag
       etag
     }
-  }
-
-  def Cached(lastModified: Date)(result: => PlainResult)(implicit request: RequestHeader): Result = {
-    if (current.mode == Mode.Dev) {
-      result
-    } else {
-      val resourceEtag = etagFor(request.uri + " - " + lastModified.getTime)
-      request.headers.get(IF_NONE_MATCH) match {
-        case Some(etag) if etag == resourceEtag => NotModified
-        case _ => play.api.cache.Cache.getOrElse(resourceEtag) {
-          result.withHeaders(ETAG -> resourceEtag)
-        }
-      }
-    }
-  }
-
-  def Cached(result: => PlainResult)(implicit request: RequestHeader): Result = {
-    Cached(new Date(0))(result)
   }
 }
--- app/controllers/CacheNotifications.scala
+++ app/controllers/CacheNotifications.scala
@@ -2,12 +2,13 @@ package controllers
 
 import notifications.Notifications
 import play.api.mvc.{RequestHeader, PlainResult}
+import play.api.i18n.Lang
 import java.util.Date
 
 trait CacheNotifications extends Cache {
   this: Notifications =>
   
-  abstract override def Cached(lastModified: Date)(result: => PlainResult)(implicit request: RequestHeader) = {
+  abstract override def Cached(lastModified: Date)(result: => PlainResult)(implicit request: RequestHeader, lang: Lang) = {
     flashNotifications match {
       case Some(_) => result
       case None    => super.Cached(lastModified)(result)
