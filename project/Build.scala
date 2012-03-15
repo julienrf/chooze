b945ff5001add9587724607cb9f4e65996c4cc56
Julien Richard-Foy <julien@richard-foy.fr>
2012-03-15 22:40:45 +0100

    Add postgresql as a dependency, for heroku

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
-      "org.scalaquery" % "scalaquery_2.9.0-1" % "0.9.5"
+      "org.scalaquery" % "scalaquery_2.9.0-1" % "0.9.5",
+      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
