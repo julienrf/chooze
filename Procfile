6b11dc6816cb6178a5b0d8e653d966097cc61e6f
Julien Richard-Foy <julien@richard-foy.fr>
2012-03-16 00:00:24 +0100

    Try to use mysql...

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: target/start -Dhttp.port=$PORT -DapplyEvolutions.default=true -Ddb.default.driver=org.postgresql.Driver -Ddb.default.url=$DATABASE_URL
+web: target/start -Dhttp.port=$PORT -DapplyEvolutions.default=true -Ddb.default.driver=com.mysql.jdbc.Driver -Ddb.default.url=$CLEARDB_DATABASE_URL
--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "org.scalaquery" % "scalaquery_2.9.0-1" % "0.9.5",
-      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+      "mysql" % "mysql-connector-java" % "5.1.18"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
