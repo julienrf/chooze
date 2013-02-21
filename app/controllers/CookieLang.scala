01ddf595d9b917b305fafb805d39078e5b73b06f
Julien Richard-Foy <julien@richard-foy.fr>
2013-02-21 18:01:33 +0100

    Use .withLang

--- app/controllers/CookieLang.scala
+++ app/controllers/CookieLang.scala
@@ -1,17 +1,17 @@
 package controllers
 
-import play.api.mvc.{Action, Controller, Cookie, RequestHeader}
+import play.api.mvc.{Action, Controller}
 import play.api.i18n.Lang
-import play.api.data.Form
 import play.api.data._
 import play.api.data.Forms._
 import play.api.Logger
+import play.api.Play.current
 
 trait CookieLang extends Controller {
 
   val localeForm = Form("locale" -> nonEmptyText)
 
-  def changeLocale = Action { implicit request =>
+  val changeLocale = Action { implicit request =>
     val referrer = request.headers.get(REFERER).getOrElse(HOME_URL)
     localeForm.bindFromRequest.fold(
       errors => {
@@ -20,17 +20,9 @@ trait CookieLang extends Controller {
       },
       locale => {
         Logger.logger.debug("Change user lang to : " + locale)
-        Redirect(referrer).withCookies(Cookie(LANG, locale))
+        Redirect(referrer).withLang(Lang(locale)) // TODO Check if the lang is handled by the application
       })
   }
 
-  override implicit def lang(implicit request: RequestHeader) = {
-    request.cookies.get(LANG) match {
-      case None => super.lang(request)
-      case Some(cookie) => Lang(cookie.value)
-    }
-  }
-
-  private val LANG = "lang"
   protected val HOME_URL = "/"
 }
