6e69a60221562dfa97fb92aa754de204ee035223
Julien Richard-Foy <julien@richard-foy.fr>
2014-02-18 20:34:33 +0100

    fix anti-flood

--- app/assets/javascripts/anti-flood.js
+++ app/assets/javascripts/anti-flood.js
@@ -1,11 +1,14 @@
 define(function () {
-  var form = document.querySelector('form');
-  if (form !== null) {
+  var forms = document.querySelectorAll('form');
+  for (var i = 0, l = forms.length ; i < l ; i++) {
+    var form = forms.item(i);
     var buttons = form.querySelectorAll('input[type=submit], button:not([type=button])');
-    form.addEventListener('submit', function () {
-      for (var i = 0, l = buttons.length ; i < l ; i++) {
-        buttons.item(i).disabled = true;
+    form.addEventListener('submit', (function (buttons) {
+      return function () {
+        for (var j = 0, m = buttons.length ; j < m ; j++) {
+          buttons.item(j).disabled = true;
+        }
       }
-    });
+    })(buttons));
   }
 });
